0c0a0cdef7307158ffa6247551b2028d82592745
Heo <gjgudrn1114@naver.com>
2023-08-04 17:29:27 +0900

    상품 진열

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,28 +1,53 @@
 <template>
     <div class="card shadow-sm">
-        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        <span class="painting" :style="{backgroundImage: `url(${item.imgPath})`}"/>
         <div class="card-body">
-        <p class="card-text">{{ item }}</p>
+        <p class="card-text">
+            <span>{{ item.name }} &nbsp;</span>
+            <span class="discount badge bg-danger">
+                    {{ item.discountPer }}% 할인
+            </span>
+        </p>
         <div class="d-flex justify-content-between align-items-center">
-            <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-            </div>
-            <small class="text-body-secondary">9 mins</small>
+            <button class="btn btn-primary">구입</button>
+            <small class="price text-muted">
+                ${{ lib.getNumberFormatted(item.price) }}
+            </small>
+            <small class="real text-danger">
+                ${{ lib.getNumberFormatted(item.price * (1 - 0.01*item.discountPer)) }}
+            </small>
         </div>
         </div>
     </div>
 </template>
 
 <script>
+import lib from "@/scripts/lib";
+
 export default {
     name: "Card",
 
     props:{
         item: String
+    },
+    setup(){
+        return {lib}
+        // lib 라이브러리 사용
     }
 }
 </script>
 
 <style scoped>
+.card .painting {
+    display:inline-block;
+    width: 100%;
+    height: 300px;
+    background-size: cover;
+    background-position: center;
+}
+
+.card .card-body .price {
+    text-decoration: line-through;
+    margin-left: 35%
+}
 </style>
