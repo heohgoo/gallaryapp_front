de96621d2aa6189f351dd4b7636cfaa5f293700d
Heo <gjgudrn1114@naver.com>
2023-08-04 18:19:07 +0900

    router 추가/로그인 화면 구성

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
-    <Header/>
-    <Home/>
-    <Footer/>
-    
+    <Header />
+    <RouterView />
+    <Footer />   
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Home from './pages/Home.vue';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 
 export default {
   name: 'App',
   components: {
     Header,
-    Footer,
-    Home
+    Footer
 }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Home from '@/pages/Home'
+import Login from '@/pages/Login'
+import {createRouter, createWebHistory} from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/login',
+        component: Login,
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
