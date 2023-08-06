f9898f02997705c5425d5cc797f8264e3822757f
Heo <gjgudrn1114@naver.com>
2023-08-06 18:57:48 +0900

    Vuex로 로그인 보완

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 <script>
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import store from './scripts/store';
 
 
 export default {
@@ -14,7 +15,15 @@ export default {
   components: {
     Header,
     Footer
-}
+  },
+
+  setup() {
+    const id = sessionStorage.getItem("id");
+
+    if (id) {
+      store.commit("setAccount", id);
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,23 +1,7 @@
 import { createApp } from 'vue'
+import store from "@/scripts/store"
 import App from './App.vue'
-import Home from '@/pages/Home'
-import Login from '@/pages/Login'
-import {createRouter, createWebHistory} from 'vue-router'
+import router from '@/scripts/router'
 
-const routes = [
-    {
-        path: '/',
-        component: Home,
-    },
-    {
-        path: '/login',
-        component: Login,
-    }
-]
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
