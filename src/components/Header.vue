9dd12a9c0ef90102ee44b84ab6fc960f2125aeb5
Heo <gjgudrn1114@naver.com>
2023-08-10 09:32:58 +0900

    카트 제약조건 추가

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -28,7 +28,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
             <strong>Gallery</strong>
           </router-link>
-          <router-link to="/cart" class="cart btn">
+          <router-link to="/cart" class="cart btn" v-if="$store.state.account.id">
             <i class="fa fa-shopping-cart" aria-hidden="true"></i>
           </router-link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
